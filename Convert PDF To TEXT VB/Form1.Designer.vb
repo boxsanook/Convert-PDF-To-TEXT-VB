3202d4dbd3465ae1b52c1098a69440607972fe08
boxsanook <37097193+boxsanook@users.noreply.github.com>
2023-06-10 17:40:22 +0700

    New

--- Convert PDF To TEXT VB/Form1.Designer.vb
+++ Convert PDF To TEXT VB/Form1.Designer.vb
@@ -22,10 +22,102 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BrowsePDF = New System.Windows.Forms.Button()
+        Me.TXT_PDF = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.BrowsePDF)
+        Me.GroupBox1.Controls.Add(Me.TXT_PDF)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(22, 11)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GroupBox1.Size = New System.Drawing.Size(1088, 90)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'BrowsePDF
+        '
+        Me.BrowsePDF.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BrowsePDF.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BrowsePDF.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.BrowsePDF.ForeColor = System.Drawing.Color.White
+        Me.BrowsePDF.Location = New System.Drawing.Point(955, 23)
+        Me.BrowsePDF.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BrowsePDF.Name = "BrowsePDF"
+        Me.BrowsePDF.Size = New System.Drawing.Size(121, 31)
+        Me.BrowsePDF.TabIndex = 2
+        Me.BrowsePDF.Text = "Browse File"
+        Me.BrowsePDF.UseVisualStyleBackColor = True
+        '
+        'TXT_PDF
+        '
+        Me.TXT_PDF.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TXT_PDF.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TXT_PDF.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TXT_PDF.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.TXT_PDF.ForeColor = System.Drawing.Color.White
+        Me.TXT_PDF.Location = New System.Drawing.Point(144, 24)
+        Me.TXT_PDF.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TXT_PDF.Multiline = True
+        Me.TXT_PDF.Name = "TXT_PDF"
+        Me.TXT_PDF.ReadOnly = True
+        Me.TXT_PDF.Size = New System.Drawing.Size(793, 25)
+        Me.TXT_PDF.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(17, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(121, 25)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Load PDF :"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox1.Location = New System.Drawing.Point(22, 116)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(1088, 528)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(30, Byte), Integer), CType(CType(45, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1122, 656)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Convert PDF To Text"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents BrowsePDF As Button
+    Friend WithEvents TXT_PDF As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
 End Class
--- Convert PDF To TEXT VB/Form1.vb
+++ Convert PDF To TEXT VB/Form1.vb
@@ -1,3 +1,58 @@
-﻿Public Class Form1
+﻿Imports System.Text
+Imports iTextSharp.text.pdf
+Imports iTextSharp.text.pdf.parser
 
+Public Class Form1
+
+    Public Function ConvertPdfToText(ByVal pdfPath As String) As String
+        ' Read the PDF file
+        Dim reader As New PdfReader(pdfPath)
+        Dim RichTextBoxPDF As New RichTextBox
+        ' Initialize a StringBuilder to store the extracted text
+        Dim textBuilder As New StringBuilder()
+
+        ' Iterate through each page of the PDF
+        For page As Integer = 1 To reader.NumberOfPages
+            ' Extract the text from the current page
+            Dim strategy As New SimpleTextExtractionStrategy()
+            Dim currentPageText As String = PdfTextExtractor.GetTextFromPage(reader, page, strategy)
+            currentPageText = currentPageText.Replace(vbNullChar, "")
+            Dim lines As String() = currentPageText.Split({vbCrLf, vbCr, vbLf}, StringSplitOptions.RemoveEmptyEntries)
+            For Each chunk As String In lines
+                RichTextBoxPDF.AppendText(chunk & Environment.NewLine)
+            Next
+            'textBuilder.AppendLine(extractedText.ToString())
+            RichTextBoxPDF.AppendText("-------------------------------------------- Page No. :" & page & "-------------------------------------------------------" & vbNewLine)
+            ' Append the extracted text to the StringBuilder
+            'textBuilder.AppendLine(currentPageText)
+        Next
+
+        ' Close the PDF reader
+        reader.Close()
+
+        ' Return the extracted text as a string
+        Return RichTextBoxPDF.Text
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BrowsePDF_Click(sender As Object, e As EventArgs) Handles BrowsePDF.Click
+        Dim loadFile As New OpenFileDialog()
+        loadFile.Filter = "PDF Files (*.pdf)|*.pdf"
+        loadFile.Title = "Select a PDF File"
+        TXT_PDF.Text = ""
+
+        Try
+            If loadFile.ShowDialog() = DialogResult.OK Then
+                Dim selectedFilePath As String = loadFile.FileName
+                RichTextBox1.Text = ConvertPdfToText(selectedFilePath)
+                TXT_PDF.Text = selectedFilePath
+            End If
+        Catch ex As Exception
+
+        End Try
+
+
+    End Sub
 End Class
